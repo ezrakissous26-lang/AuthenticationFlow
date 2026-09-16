65b1f8f0847f7a6acd2715acbd9a28963879d79c
Ezra <ezrakissous26@gmail.com>
2026-09-17 01:00:42 +0300

    feat: store authentication token with Zustand

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,18 +1,26 @@
 import { useState } from "react"
 import { login } from "../services/api"
+import { useAuthStore } from "../store/authStore"
 
 export default function Login() {
     const [ email, setEmail ] = useState('')
     const [ password, setPassword ] = useState('')
+    const setToken = useAuthStore((state) => state.setToken)
+    const [ message, setMessage ] = useState('')
   return (
-    <form onSubmit={(e) => {
+    <form onSubmit={ async (e) => {
         e.preventDefault()
-        login(email, password)
+        const data = ( await login(email, password))
+        setToken(data.token)
+        setMessage(data.message)
         }}>
+
         <input onChange={(e) =>  setEmail(e.target.value)} type="email" value={email} name="" id="" placeholder="Enter your e-mail"/>
         <input onChange={(e) => setPassword(e.target.value)} type="password" value={password} name="" id="" placeholder="Enter your password"/>
         <button type="reset">Reset</button>
         <button type="submit">Submit</button>
+        <p>{message}</p>
+
     </form>
   )
 }
